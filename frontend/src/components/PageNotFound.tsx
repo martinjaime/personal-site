2a8c60688762996b4738fa7a1848d6ceb66bc2aa
Martin Jaime <mrtnjaime@gmail.com>
2024-05-31 10:41:36 -0700

    add router with PageNotFound page

---
+++ frontend/src/components/PageNotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PageNotFound: React.FC = () => (
+  <div className='text-center text-white'>
+    <h1>Page Not Found</h1>
+    <p>Are you sure this is the page you were looking for?</p>
+  </div>
+);
+
+export default PageNotFound;
