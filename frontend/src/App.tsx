fc63c147367e2ae79baff072044382820f992bd9
Martin Jaime <mrtnjaime@gmail.com>
2024-05-31 22:23:20 -0700

    Wishlist page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import PageNotFound from './components/PageNotFound';
+import WishList from './pages/WishList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/wishlist" element={<WishList />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
